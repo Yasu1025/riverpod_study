356aa2f3b18ea0d042f22e642f5a4f6820b554a2
Yasu1025 <sillyjp25@gmail.com>
2023-01-07 17:42:57 +0900

    add count logic and test

---
+++ test/count_logic_test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod_study/data/count/count_data.dart';
+import 'package:riverpod_study/logic/count_logic.dart';
+
+void main() {
+  CountLogic target = CountLogic();
+
+  setUp(() async {
+    target = CountLogic();
+  });
+
+  test('init', () async {
+    expect(
+      target.countData,
+      const CountData(count: 0, countUp: 0, countDown: 0),
+    );
+  });
+  test('inincreaseit', () async {
+    target.increase();
+    expect(
+      target.countData,
+      const CountData(count: 1, countUp: 1, countDown: 0),
+    );
+    target.increase();
+    target.increase();
+    target.increase();
+    expect(
+      target.countData,
+      const CountData(count: 4, countUp: 4, countDown: 0),
+    );
+  });
+  test('decrease', () async {
+    target.decrease();
+    expect(
+      target.countData,
+      const CountData(count: -1, countUp: 0, countDown: 1),
+    );
+    target.decrease();
+    target.decrease();
+    target.decrease();
+    expect(
+      target.countData,
+      const CountData(count: -4, countUp: 0, countDown: 4),
+    );
+  });
+  test('reset', () async {
+    target.increase();
+    target.decrease();
+    target.increase();
+    target.increase();
+
+    target.reset();
+    expect(
+      target.countData,
+      const CountData(count: 0, countUp: 0, countDown: 0),
+    );
+  });
+}
